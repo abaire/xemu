34db988db85c4f7a91c260b098270c2349f4445b
Erik Abair <erik.abair@gmail.com>
2021-12-22 08:52:10 -0800

    Add a basic nv2a debugger

--- hw/xbox/nv2a/debug.c
+++ hw/xbox/nv2a/debug.c
@@ -208,3 +208,280 @@ void nv2a_dbg_renderdoc_capture_frames(uint32_t num_frames) {
 #endif
 
 #endif // DEBUG_NV2A_GL
+
+#ifdef ENABLE_NV2A_DEBUGGER
+#include <memory.h>
+#include "nv2a_int.h"
+#include "qemu/osdep.h"
+#include "qemu/thread.h"
+#include "sysemu/runstate.h"
+
+enum NV2A_DBG_STATE {
+    NV2A_DBG_RUNNING = 0,
+    NV2A_DBG_STOPPED_FRAMEBUFFER_SWAP,
+    NV2A_DBG_STOPPED_BEGIN_END
+};
+
+typedef struct NV2ADebuggerVMState {
+    // Indicates that the VM should be paused at the next framebuffer swap.
+    uint32_t frame_break_requested;
+    // Indicates that the VM should be paused after N begin_end::end operations.
+    uint32_t draw_end_break_requested;
+
+    enum NV2A_DBG_STATE debugger_state;
+
+    struct NV2AState* device;
+} NV2ADebuggerVMState;
+
+static NV2ADebuggerVMState g_debugger_state;
+static NV2ADbgState g_nv2a_info;
+
+typedef struct NV2ADebuggerTextureState {
+    GLuint texture;
+    GLint internal_format;
+    GLint width;
+    GLint height;
+    GLenum format;
+    GLenum type;
+} NV2ADebuggerTextureState;
+
+#define MAX_TEXTURE_INFOS 512
+static NV2ADebuggerTextureState g_texture_info[MAX_TEXTURE_INFOS] = {0};
+
+static uint32_t g_frame_counter = 0;
+static uint32_t g_draw_ends_since_last_frame = 0;
+void (*nv2a_dbg_on_frame_stepped)(void) = NULL;
+
+void nv2a_dbg_initialize(struct NV2AState* device)
+{
+    memset(&g_debugger_state, 0, sizeof(g_debugger_state));
+    g_debugger_state.device = device;
+
+    memset(&g_nv2a_info, 0, sizeof(g_nv2a_info));
+    g_nv2a_stats.vram_ptr = device->vram_ptr;
+    g_nv2a_stats.ramin_ptr = device->ramin_ptr;
+    g_nv2a_stats.pfifo_regs = device->pfifo.regs;
+}
+
+static void resume_vm(void)
+{
+    enum NV2A_DBG_STATE run_state = qatomic_read(
+            &g_debugger_state.debugger_state);
+    if (run_state != NV2A_DBG_RUNNING) {
+        qatomic_set(&g_debugger_state.debugger_state, NV2A_DBG_RUNNING);
+        qatomic_set(
+                &g_debugger_state.device->pgraph.waiting_for_nv2a_debugger,
+                false);
+        vm_start();
+    }
+    g_nv2a_info.draw_info.last_draw_operation = NV2A_DRAW_TYPE_INVALID;
+}
+
+void nv2a_dbg_step_frame(void)
+{
+    if (qatomic_read(&g_debugger_state.debugger_state) != NV2A_DBG_RUNNING) {
+        qatomic_set(&g_debugger_state.frame_break_requested, true);
+        resume_vm();
+    } else {
+        qatomic_set(&g_debugger_state.frame_break_requested, true);
+    }
+}
+
+void nv2a_dbg_step_begin_end(uint32_t num_draw_calls)
+{
+    if (qatomic_read(&g_debugger_state.debugger_state) != NV2A_DBG_RUNNING) {
+        qatomic_set(&g_debugger_state.draw_end_break_requested, num_draw_calls);
+        resume_vm();
+    } else {
+        qatomic_set(&g_debugger_state.draw_end_break_requested, num_draw_calls);
+    }
+}
+
+void nv2a_dbg_continue(void)
+{
+    qatomic_set(&g_debugger_state.frame_break_requested, false);
+    qatomic_set(&g_debugger_state.draw_end_break_requested, false);
+    resume_vm();
+}
+
+static void pause_vm(void)
+{
+    qemu_system_vmstop_request_prepare();
+    qemu_system_vmstop_request(RUN_STATE_PAUSED);
+    qatomic_set(
+        &g_debugger_state.device->pgraph.waiting_for_nv2a_debugger,
+        true);
+}
+
+void nv2a_dbg_handle_frame_swap(void)
+{
+    ++g_frame_counter;
+    g_draw_ends_since_last_frame = 0;
+    g_nv2a_info.draw_info.frame_counter = g_frame_counter;
+    g_nv2a_info.draw_info.draw_ends_since_last_frame = g_draw_ends_since_last_frame;
+
+    if (!qatomic_read(&g_debugger_state.frame_break_requested) ||
+        qatomic_read(&g_debugger_state.debugger_state) != NV2A_DBG_RUNNING) {
+
+        return;
+    }
+
+    pause_vm();
+
+    qatomic_set(&g_debugger_state.frame_break_requested, false);
+    qatomic_set(&g_debugger_state.debugger_state,
+                NV2A_DBG_STOPPED_FRAMEBUFFER_SWAP);
+
+    if (nv2a_dbg_on_frame_stepped) {
+        nv2a_dbg_on_frame_stepped();
+    }
+}
+
+void nv2a_dbg_handle_begin_end(NV2ADbgDrawInfo* info)
+{
+    PGRAPHState* pg = &g_debugger_state.device->pgraph;
+
+    info->frame_counter = g_frame_counter;
+    ++g_draw_ends_since_last_frame;
+    info->draw_ends_since_last_frame = g_draw_ends_since_last_frame;
+
+    info->fixed_function = GET_MASK(pg->regs[NV_PGRAPH_CSV0_D],
+                                    NV_PGRAPH_CSV0_D_MODE) == 0;
+    info->lighting_enabled = GET_MASK(pg->regs[NV_PGRAPH_CSV0_C],
+                                      NV_PGRAPH_CSV0_C_LIGHTING);
+
+    for (int i = 0; i < NV2A_DBG_VERTEX_ATTR__COUNT; ++i) {
+        NV2ADbgVertexInfo* dst = &info->vertex_attributes[i];
+        VertexAttribute* src = &pg->vertex_attributes[i];
+
+        dst->format = src->format;
+        dst->size = src->size;
+        dst->count = src->count;
+        dst->stride = src->stride;
+        memcpy(dst->inline_value, src->inline_value, sizeof(dst->inline_value));
+    }
+
+    g_nv2a_info.draw_info = *info;
+    uint32_t draw_end_break_count =
+        qatomic_read(&g_debugger_state.draw_end_break_requested);
+    if (!draw_end_break_count ||
+        qatomic_read(&g_debugger_state.debugger_state) != NV2A_DBG_RUNNING) {
+        return;
+    }
+
+    --draw_end_break_count;
+    qatomic_set(&g_debugger_state.draw_end_break_requested, draw_end_break_count);
+    if (draw_end_break_count) {
+        return;
+    }
+
+    pause_vm();
+    qatomic_set(&g_debugger_state.debugger_state, NV2A_DBG_STOPPED_BEGIN_END);
+}
+
+void nv2a_dbg_handle_generate_texture(GLuint texture,
+                                      GLint internal_format,
+                                      uint32_t width,
+                                      uint32_t height,
+                                      GLenum format,
+                                      GLenum type)
+{
+    NV2ADebuggerTextureState* info = g_texture_info;
+    for (uint32_t i = 0; i < MAX_TEXTURE_INFOS; ++i, ++info) {
+        if (info->texture && info->texture != texture) {
+            continue;
+        }
+
+        info->texture = texture;
+        info->internal_format = internal_format;
+        info->width = (GLint)width;
+        info->height = (GLint)height;
+        info->format = format;
+        info->type = type;
+        return;
+    }
+
+    printf("nv2a_dbg_handle_generate_texture: ran out of info slots.\n");
+}
+
+void nv2a_dbg_handle_delete_texture(GLuint texture)
+{
+    NV2ADebuggerTextureState* info = g_texture_info;
+    for (uint32_t i = 0; i < MAX_TEXTURE_INFOS; ++i, ++info) {
+        if (info->texture == texture) {
+            info->texture = 0;
+            return;
+        }
+    }
+
+    // TODO: Resolve this problem. This definitely happens in real usage.
+//    printf("nv2a_dbg_handle_delete_texture: failed to delete texture info.\n");
+}
+
+static NV2ADebuggerTextureState* find_texture_info(GLuint texture)
+{
+    NV2ADebuggerTextureState* info = g_texture_info;
+    for (uint32_t i = 0; i < MAX_TEXTURE_INFOS; ++i, ++info) {
+        if (info->texture == texture) {
+            return info;
+        }
+    }
+    return NULL;
+}
+
+NV2ADbgState* nv2a_dbg_fetch_state(void)
+{
+    nv2a_dbg_free_state(&g_nv2a_info);
+
+    PGRAPHState* pg = &g_debugger_state.device->pgraph;
+
+    g_nv2a_info.draw_info.primitive_mode = pg->primitive_mode;
+    g_nv2a_info.backbuffer_width = pg->surface_binding_dim.width;
+    g_nv2a_info.backbuffer_height = pg->surface_binding_dim.height;
+
+    g_nv2a_info.draw_info.depth_buffer_float_mode = pg->surface_shape.z_format;
+    g_nv2a_info.draw_info.depth_buffer_mode = pg->surface_shape.zeta_format;
+
+    NV2ADbgTextureInfo* tex_info = g_nv2a_info.textures;
+    for (int i = 0; i < NV2A_MAX_TEXTURES; ++i) {
+        uint32_t ctl_0 = pg->regs[NV_PGRAPH_TEXCTL0_0 + i*4];
+        bool enabled = GET_MASK(ctl_0, NV_PGRAPH_TEXCTL0_0_ENABLE);
+        if (!enabled) {
+            continue;
+        }
+
+        TextureBinding* binding = pg->texture_binding[i];
+
+        tex_info->slot = i;
+        tex_info->target = binding->gl_target;
+        tex_info->texture = binding->gl_texture;
+
+        NV2ADebuggerTextureState* state = find_texture_info(
+            binding->gl_texture);
+        if (!tex_info) {
+            printf("nv2a_dbg_fetch_state: Failed to look up texture %d\n",
+                   binding->gl_texture);
+            // The texture can probably still be rendered, use some defaults.
+            tex_info->width = 64;
+            tex_info->height = 64;
+        } else {
+            tex_info->width = state->width;
+            tex_info->height = state->height;
+        }
+
+        ++tex_info;
+    }
+    return &g_nv2a_info;
+}
+
+void nv2a_dbg_free_state(NV2ADbgState* state)
+{
+    memset(state->textures, 0, sizeof(state->textures));
+}
+
+void nv2a_dbg_invalidate_shader_cache(void)
+{
+    PGRAPHState* pg = &g_debugger_state.device->pgraph;
+    g_hash_table_remove_all(pg->shader_cache);
+}
+#endif // ENABLE_NV2A_DEBUGGER
--- hw/xbox/nv2a/debug.h
+++ hw/xbox/nv2a/debug.h
@@ -36,7 +36,10 @@
 # define NV2A_DPRINTF(format, ...)       do { } while (0)
 #endif
 
-// #define DEBUG_NV2A_GL
+// Enable debugger functionality within xemu.
+#define ENABLE_NV2A_DEBUGGER
+
+#define DEBUG_NV2A_GL
 #ifdef DEBUG_NV2A_GL
 
 #include <stdbool.h>
@@ -151,6 +154,10 @@ typedef struct NV2AStats {
         int counters[NV2A_PROF__COUNT];
     } frame_working, frame_history[NV2A_PROF_NUM_FRAMES];
     unsigned int frame_ptr;
+
+    const unsigned char *vram_ptr;
+    const unsigned char *ramin_ptr;
+    const unsigned int *pfifo_regs;
 } NV2AStats;
 
 #ifdef __cplusplus
@@ -161,6 +168,201 @@ extern NV2AStats g_nv2a_stats;
 
 const char *nv2a_profile_get_counter_name(unsigned int cnt);
 int nv2a_profile_get_counter_value(unsigned int cnt);
+
+unsigned int nv2a_get_ramht_offset(void);
+unsigned int nv2a_get_ramht_size(void);
+
+#ifdef ENABLE_NV2A_DEBUGGER
+#include <epoxy/gl.h>
+
+struct NV2AState;
+
+typedef struct NV2ADbgTextureInfo {
+    uint32_t slot;
+    GLuint target;
+    GLuint texture;
+    GLint width;
+    GLint height;
+} NV2ADbgTextureInfo;
+
+enum NV2A_DRAW_TYPE {
+    NV2A_DRAW_TYPE_INVALID,
+    NV2A_DRAW_TYPE_DRAW_ARRAYS,
+    NV2A_DRAW_TYPE_INLINE_BUFFERS,
+    NV2A_DRAW_TYPE_INLINE_ARRAYS,
+    NV2A_DRAW_TYPE_INLINE_ELEMENTS,
+    NV2A_DRAW_TYPE_EMPTY
+};
+
+typedef struct NV2ADbgVertexInfo {
+    unsigned int format;
+    unsigned int size; /* size of the data type */
+    unsigned int count; /* number of components */
+    uint32_t stride;
+
+    float inline_value[4];
+} NV2ADbgVertexInfo;
+
+// TODO: make nv2a_reg.h includable from C++.
+enum NV2A_DBG_VERTEX_ATTR {
+    NV2A_DBG_VERTEX_ATTR_POSITION = 0,
+    NV2A_DBG_VERTEX_ATTR_WEIGHT,
+    NV2A_DBG_VERTEX_ATTR_NORMAL,
+    NV2A_DBG_VERTEX_ATTR_DIFFUSE,
+    NV2A_DBG_VERTEX_ATTR_SPECULAR,
+    NV2A_DBG_VERTEX_ATTR_FOG,
+    NV2A_DBG_VERTEX_ATTR_POINT_SIZE,
+    NV2A_DBG_VERTEX_ATTR_BACK_DIFFUSE,
+    NV2A_DBG_VERTEX_ATTR_BACK_SPECULAR,
+    NV2A_DBG_VERTEX_ATTR_TEXTURE0,
+    NV2A_DBG_VERTEX_ATTR_TEXTURE1,
+    NV2A_DBG_VERTEX_ATTR_TEXTURE2,
+    NV2A_DBG_VERTEX_ATTR_TEXTURE3,
+    NV2A_DBG_VERTEX_ATTR__COUNT
+};
+
+typedef struct NV2ADbgTextureConfigInfo {
+  union {
+    struct {
+      uint32_t COLOR_KEY_OP : 2;
+      uint32_t ALPHA_KILL_ENABLE : 1;
+      uint32_t IMAGE_FIELD_ENABLE : 1;
+      uint32_t MAX_ANISO : 2;
+      uint32_t MAX_LOD_CLAMP : 12;
+      uint32_t MIN_LOD_CLAMP : 12;
+      uint32_t ENABLE : 2;
+    } bv;
+    uint32_t v;
+  } control0;
+
+  union {
+    struct {
+      uint32_t _RESERVED : 16;
+      uint32_t PITCH : 16;
+    } bv;
+    uint32_t v;
+  } control1;
+
+  union {
+    struct {
+      uint32_t U : 4;
+      uint32_t CYLWRAP_U : 4;
+      uint32_t V : 4;
+      uint32_t CYLWRAP_V : 4;
+      uint32_t P : 4;
+      uint32_t CYLWRAP_P : 4;
+      uint32_t CYLWRAP_Q : 4;
+    } bv;
+    uint32_t v;
+  } address;
+
+  union {
+    struct {
+      uint32_t _RESERVED0 : 1;
+      uint32_t CONTEXT_DMA : 1;
+      uint32_t CUBEMAP_ENABLE : 1;
+      uint32_t BORDER_SOURCE : 1;
+      uint32_t _RESERVED1 : 2;
+      uint32_t DIMENSIONALITY : 2;
+      uint32_t COLOR : 7;
+      uint32_t _RESERVED2 : 1;
+      uint32_t MIPMAP_LEVELS : 4;
+      uint32_t BASE_SIZE_U : 4;
+      uint32_t BASE_SIZE_V : 4;
+      uint32_t BASE_SIZE_P : 4;
+    } bv;
+    uint32_t v;
+  } format;
+
+  union {
+    struct {
+      uint32_t LOD_BIAS : 13;
+      uint32_t CONVOLUTION_KERNEL : 3;
+      uint32_t MIN : 8;
+      uint32_t MAG : 4;
+      uint32_t A_SIGNED : 1;
+      uint32_t R_SIGNED : 1;
+      uint32_t G_SIGNED : 1;
+      uint32_t B_SIGNED : 1;
+    } bv;
+    uint32_t v;
+  } filter;
+
+  union {
+    struct {
+      uint32_t HEIGHT : 16;
+      uint32_t WIDTH : 16;
+    } bv;
+    uint32_t v;
+  } image_rect;
+
+  uint32_t border_color;
+} NV2ADbgTextureConfigInfo;
+
+// Keep in sync with NV2A_MAX_TEXTURES
+// TODO: Make nv2a_regs.h includable from C++ files.
+#define NV2A_DEBUGGER_NUM_TEXTURES 4
+
+typedef struct NV2ADbgDrawInfo {
+    uint32_t frame_counter;
+    uint32_t draw_ends_since_last_frame;
+
+    uint32_t primitive_mode;
+
+    bool fixed_function;
+
+    // Whether lighting was enabled.
+    bool lighting_enabled;
+
+    // The type of the last performed draw operation, indicating the vertex
+    // data source.
+    enum NV2A_DRAW_TYPE last_draw_operation;
+
+    // The number of items (operation-dependent; e.g., vertices, indices) used
+    // by the last draw operation.
+    uint32_t last_draw_num_items;
+
+    uint32_t depth_buffer_mode;
+    bool depth_buffer_float_mode;
+
+    // The vertex attributes used by the last draw call.
+    NV2ADbgVertexInfo vertex_attributes[NV2A_DBG_VERTEX_ATTR__COUNT];
+
+    NV2ADbgTextureConfigInfo texture_config[4];
+
+    // The name of the texture containing the current nv2a backbuffer.
+    GLint backbuffer_texture;
+} NV2ADbgDrawInfo;
+
+typedef struct NV2ADbgState {
+    GLint backbuffer_width;
+    GLint backbuffer_height;
+    NV2ADbgTextureInfo textures[NV2A_DEBUGGER_NUM_TEXTURES];
+    NV2ADbgDrawInfo draw_info;
+} NV2ADbgState;
+
+void nv2a_dbg_initialize(struct NV2AState* device);
+void nv2a_dbg_step_frame(void);
+void nv2a_dbg_step_begin_end(uint32_t num_draw_calls);
+void nv2a_dbg_continue(void);
+void nv2a_dbg_handle_frame_swap(void);
+void nv2a_dbg_handle_begin_end(NV2ADbgDrawInfo* info);
+
+extern void (*nv2a_dbg_on_frame_stepped)(void);
+
+void nv2a_dbg_handle_generate_texture(GLuint texture,
+                                    GLint internal_format,
+                                    uint32_t width,
+                                    uint32_t height,
+                                    GLenum format,
+                                    GLenum type);
+void nv2a_dbg_handle_delete_texture(GLuint texture);
+
+NV2ADbgState* nv2a_dbg_fetch_state(void);
+void nv2a_dbg_free_state(NV2ADbgState* state);
+void nv2a_dbg_invalidate_shader_cache(void);
+
+#endif // ENABLE_NV2A_DEBUGGER
 
 #ifdef __cplusplus
 }
