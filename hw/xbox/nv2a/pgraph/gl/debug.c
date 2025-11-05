5168b0738268bfeffe0939e143f0e44bdde722e7
Erik Abair <erik.abair@gmail.com>
2025-11-04 20:46:36 -0800

    debug: Log GL errors during assertions in debug mode

--- hw/xbox/nv2a/pgraph/gl/debug.c
+++ hw/xbox/nv2a/pgraph/gl/debug.c
@@ -28,6 +28,8 @@
 #include <stdarg.h>
 #include <assert.h>
 
+#include <SDL3/SDL.h>
+
 #ifdef CONFIG_RENDERDOC
 #include "trace/control.h"
 
@@ -35,22 +37,36 @@
 #include "thirdparty/renderdoc_app.h"
 #endif
 
-#define CHECK_GL_ERROR() do { \
-  GLenum error = glGetError(); \
-  if (error != GL_NO_ERROR) {  \
-      fprintf(stderr, "OpenGL error: 0x%X (%d) at %s:%d\n", error, error, __FILE__, __LINE__); \
-      assert(!"OpenGL error detected");                                                        \
-  } \
-} while(0)
+#include "hw/xbox/nv2a/debug_gl.h"
 
-static bool has_GL_GREMEDY_frame_terminator = false;
-static bool has_GL_KHR_debug = false;
+static PFNGLPUSHDEBUGGROUPPROC _glPushDebugGroup = NULL;
+static PFNGLPOPDEBUGGROUPPROC _glPopDebugGroup = NULL;
+static PFNGLFRAMETERMINATORGREMEDYPROC _glFrameTerminatorGREMEDY = NULL;
+static PFNGLDEBUGMESSAGEINSERTPROC _glDebugMessageInsert = NULL;
+static PFNGLOBJECTLABELPROC _glObjectLabel = NULL;
+static PFNGLGETDEBUGMESSAGELOGPROC _glGetDebugMessageLog = NULL;
+
+static const char *gl_debug_type_names[] = {
+    "ERROR",  "DEPRECATED", "UNDEFINED", "PORTABILITY", "PERFORMANCE",
+    "MARKER", "PUSH_GROUP", "POP_GROUP", "OTHER",
+};
+
+static const char *gl_debug_severity_names[] = {
+    "HIGH",
+    "MEDIUM",
+    "LOW",
+    "NOTIFICATION",
+};
+
+static void APIENTRY print_gl_debug_message(GLenum source, GLenum type,
+                                            GLuint id, GLenum severity,
+                                            GLsizei length,
+                                            const GLchar *message,
+                                            const void *userParam);
 
 void gl_debug_initialize(void)
 {
-    has_GL_KHR_debug = glo_check_extension("GL_KHR_debug");
-    has_GL_GREMEDY_frame_terminator = glo_check_extension("GL_GREMEDY_frame_terminator");
-
+    bool has_GL_KHR_debug = glo_check_extension("GL_KHR_debug");
     if (has_GL_KHR_debug) {
 #if defined(__APPLE__)
         /* On macOS, calling glEnable(GL_DEBUG_OUTPUT) will result in error
@@ -63,10 +79,55 @@ void gl_debug_initialize(void)
          * debug functions which we depend on will still work as expected,
          * so skip the call for this platform.
          */
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+
+        /* epoxy's stub function sometimes loses extension context so
+         * functions are manually resolved immediately
+         */
+        _glPushDebugGroup =
+            (PFNGLPUSHDEBUGGROUPPROC)SDL_GL_GetProcAddress("glPushDebugGroup");
+        _glPopDebugGroup =
+            (PFNGLPOPDEBUGGROUPPROC)SDL_GL_GetProcAddress("glPopDebugGroup");
+        _glDebugMessageInsert =
+            (PFNGLDEBUGMESSAGEINSERTPROC)SDL_GL_GetProcAddress(
+                "glDebugMessageInsert");
+        _glObjectLabel =
+            (PFNGLOBJECTLABELPROC)SDL_GL_GetProcAddress("glObjectLabel");
+        _glGetDebugMessageLog =
+            (PFNGLGETDEBUGMESSAGELOGPROC)SDL_GL_GetProcAddress(
+                "glGetDebugMessageLog");
 #else
-       glEnable(GL_DEBUG_OUTPUT);
-       assert(glGetError() == GL_NO_ERROR);
-#endif
+        _glPushDebugGroup = glPushDebugGroup;
+        _glPopDebugGroup = glPopDebugGroup;
+        _glDebugMessageInsert = glDebugMessageInsert;
+        _glObjectLabel = glObjectLabel;
+        _glGetDebugMessageLog = glGetDebugMessageLog;
+        glEnable(GL_DEBUG_OUTPUT);
+#endif // defined(__APPLE__)
+
+        glDebugMessageCallback(print_gl_debug_message, NULL);
+        glDebugMessageControl(/* source= */ GL_DONT_CARE,
+                              /* type= */ GL_DEBUG_TYPE_ERROR,
+                              /* severity= */ GL_DONT_CARE, 0, NULL, GL_TRUE);
+        glDebugMessageControl(/* source= */ GL_DONT_CARE,
+                              /* type= */ GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR,
+                              /* severity= */ GL_DONT_CARE, 0, NULL, GL_TRUE);
+        glDebugMessageControl(/* source= */ GL_DONT_CARE,
+                              /* type= */ GL_DEBUG_TYPE_PERFORMANCE,
+                              /* severity= */ GL_DONT_CARE, 0, NULL, GL_TRUE);
+        ASSERT_NO_GL_ERROR();
+    }
+
+    bool has_GL_GREMEDY_frame_terminator =
+        glo_check_extension("GL_GREMEDY_frame_terminator");
+    if (has_GL_GREMEDY_frame_terminator) {
+#if defined(__APPLE__)
+        _glFrameTerminatorGREMEDY =
+            (PFNGLFRAMETERMINATORGREMEDYPROC)SDL_GL_GetProcAddress(
+                "glFrameTerminatorGREMEDY");
+#else
+        _glFrameTerminatorGREMEDY = glFrameTerminatorGREMEDY;
+#endif // defined(__APPLE__)
     }
 
 #ifdef CONFIG_RENDERDOC
@@ -76,7 +137,7 @@ void gl_debug_initialize(void)
 
 void gl_debug_message(bool cc, const char *fmt, ...)
 {
-    if (!has_GL_KHR_debug) {
+    if (!_glDebugMessageInsert) {
         return;
     }
 
@@ -88,8 +149,8 @@ void gl_debug_message(bool cc, const char *fmt, ...)
     assert(n <= sizeof(buffer));
     va_end(ap);
 
-    glDebugMessageInsert(GL_DEBUG_SOURCE_APPLICATION, GL_DEBUG_TYPE_MARKER,
-                         0, GL_DEBUG_SEVERITY_NOTIFICATION, n, buffer);
+    _glDebugMessageInsert(GL_DEBUG_SOURCE_APPLICATION, GL_DEBUG_TYPE_MARKER, 0,
+                          GL_DEBUG_SEVERITY_NOTIFICATION, n, buffer);
     if (cc) {
         fwrite(buffer, sizeof(char), n, stdout);
         fputc('\n', stdout);
@@ -99,7 +160,7 @@ void gl_debug_message(bool cc, const char *fmt, ...)
 void gl_debug_group_begin(const char *fmt, ...)
 {
     /* Debug group begin */
-    if (has_GL_KHR_debug) {
+    if (_glPushDebugGroup) {
         size_t n;
         char buffer[1024];
         va_list ap;
@@ -108,27 +169,27 @@ void gl_debug_group_begin(const char *fmt, ...)
         assert(n <= sizeof(buffer));
         va_end(ap);
 
-        glPushDebugGroup(GL_DEBUG_SOURCE_APPLICATION, 0, n, buffer);
+        _glPushDebugGroup(GL_DEBUG_SOURCE_APPLICATION, 0, n, buffer);
     }
 
     /* Check for errors before starting real commands in group */
-    assert(glGetError() == GL_NO_ERROR);
+    ASSERT_NO_GL_ERROR();
 }
 
 void gl_debug_group_end(void)
 {
     /* Check for errors when leaving group */
-    assert(glGetError() == GL_NO_ERROR);
+    ASSERT_NO_GL_ERROR();
 
     /* Debug group end */
-    if (has_GL_KHR_debug) {
-        glPopDebugGroup();
+    if (_glPopDebugGroup) {
+        _glPopDebugGroup();
     }
 }
 
 void gl_debug_label(GLenum target, GLuint name, const char *fmt, ...)
 {
-    if (!has_GL_KHR_debug) {
+    if (!_glObjectLabel) {
         return;
     }
 
@@ -140,19 +201,17 @@ void gl_debug_label(GLenum target, GLuint name, const char *fmt, ...)
     assert(n <= sizeof(buffer));
     va_end(ap);
 
-    glObjectLabel(target, name, n, buffer);
+    _glObjectLabel(target, name, n, buffer);
 
-    GLenum err = glGetError();
-    assert(err == GL_NO_ERROR);
+    ASSERT_NO_GL_ERROR();
 }
 
 void gl_debug_frame_terminator(void)
 {
-    CHECK_GL_ERROR();
+    ASSERT_NO_GL_ERROR();
 
 #ifdef CONFIG_RENDERDOC
     if (nv2a_dbg_renderdoc_available()) {
-
         RENDERDOC_API_1_6_0 *rdoc_api = nv2a_dbg_renderdoc_get_api();
 
         if (rdoc_api->IsTargetControlConnected()) {
@@ -162,7 +221,8 @@ void gl_debug_frame_terminator(void)
                 GLenum error = glGetError();
                 if (error != GL_NO_ERROR) {
                     fprintf(stderr,
-                            "Renderdoc EndFrameCapture triggered GL error 0x%X - ignoring\n",
+                            "Renderdoc EndFrameCapture triggered GL error 0x%X "
+                            "- ignoring\n",
                             error);
                 }
                 if (renderdoc_trace_frames) {
@@ -179,7 +239,8 @@ void gl_debug_frame_terminator(void)
                     GLenum error = glGetError();
                     if (error != GL_NO_ERROR) {
                         fprintf(stderr,
-                                "Renderdoc StartFrameCapture triggered GL error 0x%X - ignoring\n",
+                                "Renderdoc StartFrameCapture triggered GL "
+                                "error 0x%X - ignoring\n",
                                 error);
                     }
                 }
@@ -188,9 +249,142 @@ void gl_debug_frame_terminator(void)
         }
     }
 #endif
-    if (has_GL_GREMEDY_frame_terminator) {
-        glFrameTerminatorGREMEDY();
-        CHECK_GL_ERROR();
+    if (_glFrameTerminatorGREMEDY) {
+        _glFrameTerminatorGREMEDY();
+        ASSERT_NO_GL_ERROR();
+    }
+}
+
+void gl_debug_dump_log()
+{
+    if (!_glGetDebugMessageLog) {
+        return;
+    }
+
+    GLint num_messages = 0;
+    glGetIntegerv(GL_DEBUG_LOGGED_MESSAGES, &num_messages);
+    if (!num_messages) {
+        return;
+    }
+
+    GLint max_message_length = 0;
+    glGetIntegerv(GL_MAX_DEBUG_MESSAGE_LENGTH, &max_message_length);
+
+    const size_t buffer_size = num_messages * max_message_length;
+    GLchar *message_data = malloc(buffer_size);
+    GLenum *sources = calloc(num_messages, sizeof(GLenum));
+    GLenum *types = calloc(num_messages, sizeof(GLenum));
+    GLenum *severities = calloc(num_messages, sizeof(GLenum));
+    GLuint *ids = calloc(num_messages, sizeof(GLuint));
+    GLsizei *message_lengths = calloc(num_messages, sizeof(GLsizei));
+
+    if (!(message_data && sources && types && severities && ids &&
+          message_lengths)) {
+        fprintf(stderr, "Failed to alloc memory for %d GL debug messages\n",
+                num_messages);
+        goto cleanup;
+    }
+
+    GLuint retrieved =
+        _glGetDebugMessageLog(num_messages, buffer_size, sources, types, ids,
+                              severities, message_lengths, message_data);
+
+    GLchar *next_message = message_data;
+    for (GLuint i = 0; i < retrieved; ++i) {
+        print_gl_debug_message(sources[i], types[i], ids[i], severities[i],
+                               message_lengths[i], next_message, NULL);
+        next_message += message_lengths[i];
+    }
+
+cleanup:
+    if (message_data) {
+        free(message_data);
+    }
+    if (sources) {
+        free(sources);
+    }
+    if (types) {
+        free(types);
+    }
+    if (severities) {
+        free(severities);
+    }
+    if (ids) {
+        free(ids);
+    }
+    if (message_lengths) {
+        free(message_lengths);
+    }
+}
+
+static void APIENTRY print_gl_debug_message(GLenum source, GLenum type,
+                                            GLuint id, GLenum severity,
+                                            GLsizei length,
+                                            const GLchar *message,
+                                            const void *userParam)
+{
+    const char *type_name = "<UNKNOWN>";
+    const char *severity_name = "<UNKNOWN>";
+
+    if (type != GL_DEBUG_TYPE_ERROR) {
+        return;
+    }
+
+    switch (type) {
+    default:
+        break;
+    case GL_DEBUG_TYPE_ERROR:
+        type_name = gl_debug_type_names[0];
+        break;
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+        type_name = gl_debug_type_names[1];
+        break;
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+        type_name = gl_debug_type_names[2];
+        break;
+    case GL_DEBUG_TYPE_PORTABILITY:
+        type_name = gl_debug_type_names[3];
+        break;
+    case GL_DEBUG_TYPE_PERFORMANCE:
+        type_name = gl_debug_type_names[4];
+        break;
+    case GL_DEBUG_TYPE_MARKER:
+        type_name = gl_debug_type_names[5];
+        break;
+    case GL_DEBUG_TYPE_PUSH_GROUP:
+        type_name = gl_debug_type_names[6];
+        break;
+    case GL_DEBUG_TYPE_POP_GROUP:
+        type_name = gl_debug_type_names[7];
+        break;
+    case GL_DEBUG_TYPE_OTHER:
+        type_name = gl_debug_type_names[8];
+        break;
+    }
+
+    switch (severity) {
+    default:
+        break;
+    case GL_DEBUG_SEVERITY_HIGH:
+        severity_name = gl_debug_severity_names[0];
+        break;
+    case GL_DEBUG_SEVERITY_MEDIUM:
+        severity_name = gl_debug_severity_names[1];
+        break;
+    case GL_DEBUG_SEVERITY_LOW:
+        severity_name = gl_debug_severity_names[2];
+        break;
+    case GL_DEBUG_SEVERITY_NOTIFICATION:
+        severity_name = gl_debug_severity_names[3];
+        break;
+    }
+
+    if (length < 0) {
+        fprintf(stderr, "GLDBG[%s][%s]> %s\n", type_name, severity_name,
+                message);
+    } else {
+        fprintf(stderr, "GLDBG[%s][%s]> %*s\n", type_name, severity_name,
+                length, message);
     }
 }
 
