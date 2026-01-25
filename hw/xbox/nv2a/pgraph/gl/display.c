b0a6a9c5c605bec7bb304e46f1286abc20415e69
Erik Abair <erik.abair@gmail.com>
2026-01-25 06:55:34 -0800

    nv2a: Provide clearer debug names for utility FBOs

--- hw/xbox/nv2a/pgraph/gl/display.c
+++ hw/xbox/nv2a/pgraph/gl/display.c
@@ -24,6 +24,7 @@
 #include "hw/xbox/nv2a/nv2a_int.h"
 #include "hw/xbox/nv2a/pgraph/util.h"
 #include "renderer.h"
+#include "debug.h"
 
 #include <math.h>
 
@@ -101,7 +102,17 @@ void pgraph_gl_init_display(NV2AState *d)
     glGenBuffers(1, &r->disp_rndr.vbo);
     glBindBuffer(GL_ARRAY_BUFFER, r->disp_rndr.vbo);
     glBufferData(GL_ARRAY_BUFFER, 0, NULL, GL_STATIC_DRAW);
+
     glGenFramebuffers(1, &r->disp_rndr.fbo);
+    {
+        GLint prev_fbo = 0;
+        glGetIntegerv(GL_FRAMEBUFFER_BINDING, &prev_fbo);
+        glBindFramebuffer(GL_FRAMEBUFFER, r->disp_rndr.fbo);
+        NV2A_GL_DLABEL(GL_FRAMEBUFFER, r->disp_rndr.fbo,
+                       "Display renderer FBO");
+        glBindFramebuffer(GL_FRAMEBUFFER, prev_fbo);
+    }
+
     glGenTextures(1, &r->disp_rndr.pvideo_tex);
     assert(glGetError() == GL_NO_ERROR);
 
